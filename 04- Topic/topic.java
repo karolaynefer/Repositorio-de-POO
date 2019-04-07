388c8df512e757a211c85fa70a9536addf213720
Mara  Karolayne Fernandes <47923333+karolaynefer@users.noreply.github.com>
2019-04-07 19:56:26 -0300

    Update topic.java

--- 04- Topic/topic.java
+++ 04- Topic/topic.java
@@ -2,6 +2,7 @@
 package topic;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 class Passageiro{
     String id;
@@ -14,46 +15,93 @@ class Passageiro{
     
     public String toString(){
     return this.id + ":" + this.idade;
-}
-public boolean preferencial(){
-    return idade <= 60;
-}
+        }
+    public boolean preferencial(){
+        return (idade >= 60);
+    }
 }
 
-class Topic{
+class topic{
     ArrayList <Passageiro> poltronas;
     int qtdPreferenciais;
-    public Topic(int capacidade, int qtdPreferenciais){
-        poltronas = new ArrayList<Passageiros>();
+    public topic(int capacidade, int qtdPreferenciais){
+        poltronas = new ArrayList<Passageiro>();
         for(int i = 0; i < capacidade; i++){
             poltronas.add(null);
         }
-        this.qtdPreferenciais = qtdPreferenciais
+        this.qtdPreferenciais = qtdPreferenciais;
     }
     @Override
     public String toString(){
         String saida = "[";
         for(int i = 0; i < poltronas.size(); i++){
+             
             if (i < qtdPreferenciais)
                 saida += " @";
             else
-                saida += " =";        
+                saida += " =";
+           if(poltronas.get(i) != null)
+               saida += poltronas.get(i);
+            
         }
         return saida + " ]";
     }
-}
-        
-public class Topic {
     
+public void inserir(Passageiro pass){
+        if (pass.preferencial()){
+            for(int i = 0 ; i < qtdPreferenciais;i++){
+                if(poltronas.get(i) == null){
+                    poltronas.set(i, pass);
+                    return;
+                }
+            }
+            for(int i = qtdPreferenciais ; i < poltronas.size();i++){
+                if(poltronas.get(i) == null){
+                    poltronas.set(i, pass);
+                    return;
+                }
+            }
+       }else{
+                for(int i = qtdPreferenciais ; i < poltronas.size();i++){
+                if(poltronas.get(i) == null){
+                    poltronas.set(i, pass);
+                    return;
+                }
+            }
+        }
+    }
+ public void remover(String id){
+      
+        for (int i = 0;i<poltronas.size();i++){
+            if(poltronas.get(i) != null){
+                 if(poltronas.get(i).id.equals(id)){
+                    poltronas.set(i,null);
+            }
+           }
+        }
+ }
+}
+class controller {
     public static void main(String[] args) {
-        Topic topic = new topic(0, 0);
+        topic topique = new topic(0,0);
         Scanner scanner = new Scanner(System.in);
         while(true){
             String line = scanner.nextLine();
             String[] ui = line.split(" ");
+            if (ui[0].equals("sair")){
+                break;
+            }else if (ui[0].equals("init")){
+                topique = new topic(Integer.parseInt(ui[1]),Integer.parseInt(ui[2]));
+            }else if(ui[0].equals("show")){
+                System.out.println(topique);
+            }else if(ui[0].equals("in")){
+                topique.inserir(new Passageiro(ui[1],Integer.parseInt(ui[2])));
+            }else if(ui[0].equals("remov")){
+               topique.remover(ui[1]);
+            }else{
+                System.out.println("ERRO:Comando Invalido\n");
+            }
         }
     
     }
-}
-    
 }
